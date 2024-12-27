c0df2f64a6da21426390efae486f55375549bb12
kiririx <kiririx1213@gmail.com>
2024-12-27 16:46:08 +0800

    feat: 添加 V2EX 论坛爬虫功能
    - 新增 go.mod 文件，定义项目依赖- 创建 main.go 文件，实现V2EX 论坛数据爬取和展示
    - 添加 GetTabs函数，获取论坛标签页信息- 实现 GetTabContent 函数，抓取指定标签页的主题列表
    - 使用 termui 库进行终端界面展示
    - 通过 colly 库进行网页爬取

---
+++ go.mod
@@ -0,0 +1,37 @@
+module v2ex-cli
+
+go 1.23
+
+require github.com/gocolly/colly/v2 v2.1.0
+
+require (
+	github.com/PuerkitoBio/goquery v1.5.1 // indirect
+	github.com/andybalholm/cascadia v1.2.0 // indirect
+	github.com/antchfx/htmlquery v1.2.3 // indirect
+	github.com/antchfx/xmlquery v1.2.4 // indirect
+	github.com/antchfx/xpath v1.1.8 // indirect
+	github.com/gdamore/encoding v1.0.1 // indirect
+	github.com/gdamore/tcell/v2 v2.7.4 // indirect
+	github.com/gizak/termui/v3 v3.1.0 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/mitchellh/go-wordwrap v1.0.1 // indirect
+	github.com/nsf/termbox-go v1.1.1 // indirect
+	github.com/rivo/tview v0.0.0-20241103174730-c76f7879f592 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
+	github.com/spf13/cobra v1.8.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/temoto/robotstxt v1.1.1 // indirect
+	golang.org/x/net v0.6.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/term v0.27.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+	google.golang.org/protobuf v1.24.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,142 @@
+package main
+
+import (
+	"fmt"
+	ui "github.com/gizak/termui/v3"
+	"github.com/gizak/termui/v3/widgets"
+	"github.com/gocolly/colly/v2"
+	"log"
+	"strings"
+)
+
+func main() {
+	if err := ui.Init(); err != nil {
+		log.Fatalf("failed to initialize termui: %v", err)
+	}
+	defer ui.Close()
+
+	header := widgets.NewParagraph()
+	header.Text = "Press q to quit, Press h or l to switch tabs"
+	header.SetRect(0, 0, 50, 1)
+	header.Border = false
+	header.TextStyle.Bg = ui.ColorBlue
+
+	p2 := widgets.NewParagraph()
+	p2.Text = "Press q to quit\nPress h or l to switch tabs\n"
+	p2.Title = "Keys"
+	p2.SetRect(5, 5, 40, 15)
+	p2.BorderStyle.Fg = ui.ColorYellow
+
+	tabsText := GetTabs()
+	tabpane := widgets.NewTabPane(tabsText...)
+	tabpane.SetRect(0, 1, 1000, 4)
+	tabpane.Border = true
+
+	renderTab := func() {
+		// switch tabpane.ActiveTabIndex {
+		// case 0:
+		// 	ui.Render(GetTabContent(tabs[tabpane.ActiveTabIndex]))
+		// }
+		ui.Render(GetTabContent(tabs[tabpane.ActiveTabIndex].Name))
+	}
+
+	ui.Render(header, tabpane, p2)
+
+	uiEvents := ui.PollEvents()
+
+	for {
+		e := <-uiEvents
+		switch e.ID {
+		case "q", "<C-c>":
+			return
+		case "<Left>":
+			tabpane.FocusLeft()
+			ui.Clear()
+			ui.Render(header, tabpane)
+			renderTab()
+		case "<Right>":
+			tabpane.FocusRight()
+			ui.Clear()
+			ui.Render(header, tabpane)
+			renderTab()
+		}
+	}
+}
+
+type Tab struct {
+	Name string
+	Text string
+}
+
+var tabs []Tab
+
+func GetTabs() (rows []string) {
+	tabs = make([]Tab, 0)
+	defer func() {
+		rows = make([]string, 0)
+		for i, tab := range tabs {
+			rows = append(rows, fmt.Sprintf("%d. %s", i+1, tab.Text))
+		}
+	}()
+	c := colly.NewCollector()
+	c.OnHTML("#Tabs", func(e *colly.HTMLElement) {
+		e.ForEach("a", func(i int, e *colly.HTMLElement) {
+			href := e.Attr("href")
+			if strings.Contains(href, "=") {
+				tabName := href[strings.Index(href, "=")+1:]
+				tabs = append(tabs, Tab{Name: tabName, Text: e.Text})
+			}
+		})
+	})
+	c.Visit("https://www.v2ex.com/")
+	return
+}
+
+func GetTabContent(tabName string) (l *widgets.List) {
+	topics := make([]Topic, 0)
+	defer func() {
+		l = widgets.NewList()
+		l.Title = "List"
+		l.Rows = func() []string {
+			rows := make([]string, 0)
+			for i, topic := range topics {
+				rows = append(rows, fmt.Sprintf("%d.%s", i+1, topic.Text))
+			}
+			return rows
+		}()
+		l.TextStyle = ui.NewStyle(ui.ColorGreen)
+		l.WrapText = false
+		l.SetRect(0, 5, 100, 100)
+	}()
+	c := colly.NewCollector()
+
+	c.OnHTML("#Main", func(e1 *colly.HTMLElement) {
+		e1.ForEach(".cell.item", func(i int, e2 *colly.HTMLElement) {
+			topics = append(topics, Topic{
+				Text: e2.ChildText(".topic-link"),
+				Author: func() string {
+					var v string
+					e2.ForEachWithBreak("strong", func(i int, e3 *colly.HTMLElement) bool {
+						v = e3.ChildText("a")
+						return false
+					})
+					return v
+				}(),
+				Date:           e2.ChildText(".fade"),
+				ReplyCount:     e2.ChildText(".count_livid"),
+				FinalReplyUser: e2.ChildText(".last_bump"),
+			})
+		})
+	})
+
+	c.Visit("https://www.v2ex.com/?tab=" + tabName)
+	return
+}
+
+type Topic struct {
+	Text           string
+	Author         string
+	Date           string
+	FinalReplyUser string
+	ReplyCount     string
+}
